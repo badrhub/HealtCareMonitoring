4b4f0c79bf1d21b8ee45d2b2bfbf76d89107182b
badrhub <badr.jvm@gmail.com>
2023-10-28 22:16:21 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './components/details/details.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthGuard } from './auth.guard';
-import { GrapheComponent } from './components/graphe/graphe.component';
 import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { DetailsComponent } from './components/details/details.component';
+import { GrapheComponent } from './components/graphe/graphe.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'main', component: ClientsComponent  },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ClientsComponent } from './components/clients/clients.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { DetailsComponent } from './components/details/details.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthService } from './auth.service';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { AuthGuard } from './auth.guard';
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts'; 
-import { GrapheComponent } from './components/graphe/graphe.component';
+import { AuthService } from './auth.service';
 import { AboutComponent } from './components/about/about.component';
+import { DetailsComponent } from './components/details/details.component';
+import { GrapheComponent } from './components/graphe/graphe.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
